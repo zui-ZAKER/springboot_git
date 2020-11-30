17c7db6d6da59e782cc0f8ccae6c07770d695a81
zui-ZAKER <1332004207@qq.com>
2020-11-30 15:43:48 +0800

    修改

--- src/main/java/com/entor/controller/IndexController.java
+++ src/main/java/com/entor/controller/IndexController.java
@@ -1,10 +1,12 @@
 package com.entor.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IndexController {
 
+    @GetMapping("/index")
     public String index(){
         return  "Welcome to index";
     }
