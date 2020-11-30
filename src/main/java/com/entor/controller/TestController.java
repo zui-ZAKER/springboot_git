28f1efaffa1b7a779a53c17ac0604dd4f1a0f653
zui-ZAKER <1332004207@qq.com>
2020-11-30 16:39:55 +0800

    新增端口查询

--- src/main/java/com/entor/controller/TestController.java
+++ src/main/java/com/entor/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.entor.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +8,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+    @Value("${server.port}")
+    private int port;
+
     @GetMapping("/hello")
     public  String  hello(){
         return "hello";
+    }
+
+    @GetMapping("/getPort")
+    public String getPort(){
+        return "server.port="+port;
     }
 }
